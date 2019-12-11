86eeb37159fa24c875754d84a0323016dfc07369
Edward Yun <nedinsuyun@gmail.com>
2019-12-11 18:44:22 -0500

    init spring

---
+++ src/main/java/com/bestPurchasing/retail/BestPurchasingApplication.java
@@ -0,0 +1,13 @@
+package com.bestPurchasing.retail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestPurchasingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestPurchasingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bestPurchasing/retail/BestPurchasingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestPurchasing.retail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestPurchasingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
